dff32ce764eb02126b23cd9c9fd42a124b92dd0f
EnzoCouto1 <127258972+EnzoCouto1@users.noreply.github.com>
2023-06-23 22:07:07 -0300

    Create Caviar.java

---
+++ Caviar.java
@@ -0,0 +1,19 @@
+public class Caviar extends Pratos{
+    private String Adicional;
+
+    private String ModoDeServir;
+
+    public String getAdicional() {
+        return Adicional;
+    }
+
+    public void setAdicional(String adicional) {
+        Adicional = adicional;
+    }
+    public String getModoDeServir() {
+        return ModoDeServir;
+    }
+    public void setModoDeServir(String modoDeServir) {
+        ModoDeServir = modoDeServir;
+    }
+}
