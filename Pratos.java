1e2eb7aa819a37f1c2c9e714091f941831176c02
EnzoCouto1 <127258972+EnzoCouto1@users.noreply.github.com>
2023-06-23 22:05:29 -0300

    Create Pratos.java

---
+++ Pratos.java
@@ -0,0 +1,25 @@
+public class Pratos {
+    private String nomePrato;
+    private double preco;
+
+
+    public String getNomePrato() {
+        return nomePrato;
+    }
+
+    public void setNomePrato(String nomePrato) {
+        this.nomePrato = nomePrato;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void mostraInfo(){
+
+    }
+}
