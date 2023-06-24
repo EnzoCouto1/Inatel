f1ea7bfdc581a12cda4bf61b9808f951101c126c
EnzoCouto1 <127258972+EnzoCouto1@users.noreply.github.com>
2023-06-23 22:06:40 -0300

    Create CarneDeOuro.java

---
+++ CarneDeOuro.java
@@ -0,0 +1,11 @@
+public class CarneDeOuro extends Pratos{
+    private String ModelCorte;
+
+    public String getModelCorte() {
+        return ModelCorte;
+    }
+
+    public void setModelCorte(String modelCorte) {
+        ModelCorte = modelCorte;
+    }
+}
